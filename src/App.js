099a6a7c2ef6a23644afe8af67e6c1cb4ecf682b
Song <songyuseung23@gmail.com>
2019-09-10 11:31:23 +0900

    use prop-types to check types of props

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({name, picture}){
+const foodIlike = [
+  {
+    id:1,
+    name: "kimchi",
+    image: "https://minimalistbaker.com/wp-content/uploads/2016/02/EASY-10-ingredient-VEGAN-KIMCHI-Spicy-tangy-crunchy-DELICIOUS-vegan-glutenfree-recipe-kimchi-768x1152.jpg",
+    rating: 5
+  },
+  {
+    id:2,
+    name: "ramen",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190208-delish-ramen-horizontal-093-1550096715.jpg?crop=1xw:0.9995002498750624xh;center,top&resize=768:*",
+    rating: 4.6
+  },
+  {
+    id:3,
+    name: "lemon",
+    image: "https://i5.walmartimages.ca/images/Large/094/504/6000200094504.jpg",
+    rating: 4.5
+  }
+];
+
+function Food({name, picture, rating}){  //component : insert html source
   return(
     <div>
       <h1>I like {name}</h1>
+      <h4>{rating} / 5.0</h4>
       <img src={picture} alt={name}/>
     </div>
   );
 }
 
-const foodIlike = [
-  {
-    name: "kimchi",
-    image: "https://minimalistbaker.com/wp-content/uploads/2016/02/EASY-10-ingredient-VEGAN-KIMCHI-Spicy-tangy-crunchy-DELICIOUS-vegan-glutenfree-recipe-kimchi-768x1152.jpg"
-  },
-  {
-    name: "ramen",
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190208-delish-ramen-horizontal-093-1550096715.jpg?crop=1xw:0.9995002498750624xh;center,top&resize=768:*"
-  },
-  {
-    name: "lemon",
-    image: "https://i5.walmartimages.ca/images/Large/094/504/6000200094504.jpg"
-  }
-];
+Food.propTypes = {
+  name:PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
   <div>
     {foodIlike.map(dish => 
-    <Food name={dish.name} picture={dish.image}/>
+    <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
     )}
   </div>
   );
