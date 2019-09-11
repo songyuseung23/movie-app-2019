301658d8d504f25f5f8d90200076c9166dd61377
Song <songyuseung23@gmail.com>
2019-09-11 17:14:45 +0900

    Need publishing process

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"; //react uses virtual DOM
 import axios from "axios";
-import Moive from "./Movie";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -27,20 +28,31 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading.."
-          : movies.map(movie => (
-              <Moive
+      <section className="contatiner">
+        {isLoading ? (
+          <div className="loading">
+            <img
+              src="https://icon-library.net/images/loading-icon-png/loading-icon-png-19.jpg"
+              alt="loading-logo"
+            />
+            <span className="Loader-text">Loading..</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
                 key={movie.id}
                 id={movie.id}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div>
-      <h4>{title}</h4>
-      <img src={poster} alt={title} />
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie-data">
+        <h3 className="movie-title">{title}</h3>
+        <h5 className="movie-year">{year}</h5>
+        <ul className="movie-genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie-summary">{summary.slice(0, 150)}...</p>
+      </div>
     </div>
   );
 }
 
 Movie.propTypes = {
-  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
